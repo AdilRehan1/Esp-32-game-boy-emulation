c48699c81f905ef4d06cbcb1090d3276d58377fa
Adil-Rehan <146543468+AdilRehan1@users.noreply.github.com>
2026-03-16 23:12:08 +0100

    Refactor GameBoy emulator display and input handling

--- espgb.ino
+++ espgb.ino
@@ -10,66 +10,66 @@ extern "C" {
 
 #include "game_rom.h"
 
-// ---------------- DISPLAY PINS ----------------
+// ---------------- DISPLAY ----------------
 
-#define TFT_CS   5
-#define TFT_DC   2
-#define TFT_RST  4
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
 
-#define TFT_MOSI 23
-#define TFT_CLK  18
-#define TFT_MISO 19
+Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+// ---------------- BUTTONS ----------------
 
-// ---------------- BUTTON PINS ----------------
-
-#define BTN_UP     32
-#define BTN_DOWN   33
-#define BTN_LEFT   34
-#define BTN_RIGHT  35
-#define BTN_A      25
-#define BTN_B      26
-#define BTN_START  27
+#define BTN_UP 32
+#define BTN_DOWN 33
+#define BTN_LEFT 34
+#define BTN_RIGHT 35
+#define BTN_A 25
+#define BTN_B 26
+#define BTN_START 27
 #define BTN_SELECT 14
 
 // ---------------- EMULATOR ----------------
 
-gb_t gb;
+struct gb_s gb;
 
 uint16_t framebuffer[160 * 144];
 
-// ---------------- INPUT ----------------
+// ---------------- ROM ACCESS ----------------
 
-uint8_t read_input()
+uint8_t rom_read(struct gb_s* gb, const uint_fast32_t addr)
 {
-  uint8_t keys = 0;
-
-  if (!digitalRead(BTN_RIGHT)) keys |= GB_BUTTON_RIGHT;
-  if (!digitalRead(BTN_LEFT))  keys |= GB_BUTTON_LEFT;
-  if (!digitalRead(BTN_UP))    keys |= GB_BUTTON_UP;
-  if (!digitalRead(BTN_DOWN))  keys |= GB_BUTTON_DOWN;
-
-  if (!digitalRead(BTN_A))     keys |= GB_BUTTON_A;
-  if (!digitalRead(BTN_B))     keys |= GB_BUTTON_B;
-
-  if (!digitalRead(BTN_START))  keys |= GB_BUTTON_START;
-  if (!digitalRead(BTN_SELECT)) keys |= GB_BUTTON_SELECT;
-
-  return keys;
+  return game_rom[addr];
 }
 
-// ---------------- VIDEO CALLBACK ----------------
+uint8_t cart_ram_read(struct gb_s* gb, const uint_fast32_t addr)
+{
+  return 0;
+}
 
-void lcd_draw_line(struct gb_s* gb, const uint8_t* pixels, const uint_fast8_t line)
+void cart_ram_write(struct gb_s* gb, const uint_fast32_t addr, const uint8_t val)
+{
+}
+
+// ---------------- ERROR HANDLER ----------------
+
+void error(struct gb_s* gb, const enum gb_error_e err, const uint16_t addr)
+{
+  Serial.print("GB ERROR ");
+  Serial.println(err);
+}
+
+// ---------------- LCD DRAW ----------------
+
+void lcd_draw(struct gb_s* gb, const uint_fast32_t y, const uint8_t* pixels)
 {
   for (int x = 0; x < 160; x++)
   {
-    uint8_t c = pixels[x];
+    uint8_t p = pixels[x];
 
     uint16_t color;
 
-    switch (c)
+    switch (p)
     {
       case 0: color = ILI9341_WHITE; break;
       case 1: color = ILI9341_LIGHTGREY; break;
@@ -77,20 +77,33 @@ void lcd_draw_line(struct gb_s* gb, const uint8_t* pixels, const uint_fast8_t li
       default: color = ILI9341_BLACK; break;
     }
 
-    framebuffer[line * 160 + x] = color;
+    framebuffer[y * 160 + x] = color;
   }
 
-  if (line == 143)
+  if (y == 143)
   {
     tft.drawRGBBitmap(40, 20, framebuffer, 160, 144);
   }
 }
 
-// ---------------- AUDIO CALLBACK (optional) ----------------
+// ---------------- INPUT ----------------
 
-void audio_callback(struct gb_s* gb, const int16_t* samples, uint_fast8_t count)
+uint8_t read_input()
 {
-  // not implemented yet
+  uint8_t keys = 0;
+
+  if (!digitalRead(BTN_RIGHT)) keys |= GB_PAD_RIGHT;
+  if (!digitalRead(BTN_LEFT))  keys |= GB_PAD_LEFT;
+  if (!digitalRead(BTN_UP))    keys |= GB_PAD_UP;
+  if (!digitalRead(BTN_DOWN))  keys |= GB_PAD_DOWN;
+
+  if (!digitalRead(BTN_A)) keys |= GB_PAD_A;
+  if (!digitalRead(BTN_B)) keys |= GB_PAD_B;
+
+  if (!digitalRead(BTN_START))  keys |= GB_PAD_START;
+  if (!digitalRead(BTN_SELECT)) keys |= GB_PAD_SELECT;
+
+  return keys;
 }
 
 // ---------------- SETUP ----------------
@@ -113,19 +126,25 @@ void setup()
   tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
 
-  Serial.println("Starting GameBoy Emulator");
+  Serial.println("Starting GameBoy");
 
-  gb_init(&gb, lcd_draw_line, audio_callback);
+  gb_init(
+    &gb,
+    rom_read,
+    cart_ram_read,
+    cart_ram_write,
+    error,
+    NULL
+  );
 
-  gb_load_rom(&gb, game_rom);
-
+  gb_set_lcd_draw_callback(&gb, lcd_draw);
 }
 
 // ---------------- LOOP ----------------
 
 void loop()
 {
-  gb.direct.joypad = read_input();
+  gb_set_joypad(&gb, read_input());
 
   gb_run_frame(&gb);
 }
