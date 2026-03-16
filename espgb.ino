5c41133c2f75352e75a8ffd4b194993a638b672a
Adil-Rehan <146543468+AdilRehan1@users.noreply.github.com>
2026-03-16 23:11:20 +0100

    Refactor GameBoy emulator code and button handling

--- espgb.ino
+++ espgb.ino
@@ -1,102 +1,131 @@
-
+#include <Arduino.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
 extern "C" {
-  #include "peanut_gb.h"
+#define PEANUT_GB_IMPLEMENTATION
+#include "peanut_gb.h"
 }
 
 #include "game_rom.h"
 
-/* ================= TFT ================= */
+// ---------------- DISPLAY PINS ----------------
+
 #define TFT_CS   5
 #define TFT_DC   2
 #define TFT_RST  4
 
-Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
+#define TFT_MOSI 23
+#define TFT_CLK  18
+#define TFT_MISO 19
 
-/* ============== BUTTONS (FIXED) ============== */
-#define BTN_UP      32
-#define BTN_DOWN    33
-#define BTN_LEFT    12
-#define BTN_RIGHT   13
-#define BTN_A       25
-#define BTN_B       26
-#define BTN_START   27
-#define BTN_SELECT  14
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+
+// ---------------- BUTTON PINS ----------------
+
+#define BTN_UP     32
+#define BTN_DOWN   33
+#define BTN_LEFT   34
+#define BTN_RIGHT  35
+#define BTN_A      25
+#define BTN_B      26
+#define BTN_START  27
+#define BTN_SELECT 14
+
+// ---------------- EMULATOR ----------------
 
 gb_t gb;
 
-/* ============== FRAMEBUFFER ============== */
 uint16_t framebuffer[160 * 144];
 
-/* ============== DISPLAY CALLBACK ============== */
-void lcd_draw_line(uint8_t y, uint8_t *pixels) {
-  for (int x = 0; x < 160; x++) {
-    uint8_t p = pixels[x];
+// ---------------- INPUT ----------------
 
-    // Simple 4-shade grayscale palette
+uint8_t read_input()
+{
+  uint8_t keys = 0;
+
+  if (!digitalRead(BTN_RIGHT)) keys |= GB_BUTTON_RIGHT;
+  if (!digitalRead(BTN_LEFT))  keys |= GB_BUTTON_LEFT;
+  if (!digitalRead(BTN_UP))    keys |= GB_BUTTON_UP;
+  if (!digitalRead(BTN_DOWN))  keys |= GB_BUTTON_DOWN;
+
+  if (!digitalRead(BTN_A))     keys |= GB_BUTTON_A;
+  if (!digitalRead(BTN_B))     keys |= GB_BUTTON_B;
+
+  if (!digitalRead(BTN_START))  keys |= GB_BUTTON_START;
+  if (!digitalRead(BTN_SELECT)) keys |= GB_BUTTON_SELECT;
+
+  return keys;
+}
+
+// ---------------- VIDEO CALLBACK ----------------
+
+void lcd_draw_line(struct gb_s* gb, const uint8_t* pixels, const uint_fast8_t line)
+{
+  for (int x = 0; x < 160; x++)
+  {
+    uint8_t c = pixels[x];
+
     uint16_t color;
-    switch (p) {
+
+    switch (c)
+    {
       case 0: color = ILI9341_WHITE; break;
       case 1: color = ILI9341_LIGHTGREY; break;
       case 2: color = ILI9341_DARKGREY; break;
       default: color = ILI9341_BLACK; break;
     }
 
-    framebuffer[y * 160 + x] = color;
+    framebuffer[line * 160 + x] = color;
+  }
+
+  if (line == 143)
+  {
+    tft.drawRGBBitmap(40, 20, framebuffer, 160, 144);
   }
 }
 
-/* ============== SETUP BUTTONS ============== */
-void setupButtons() {
+// ---------------- AUDIO CALLBACK (optional) ----------------
+
+void audio_callback(struct gb_s* gb, const int16_t* samples, uint_fast8_t count)
+{
+  // not implemented yet
+}
+
+// ---------------- SETUP ----------------
+
+void setup()
+{
+  Serial.begin(115200);
+
   pinMode(BTN_UP, INPUT_PULLUP);
   pinMode(BTN_DOWN, INPUT_PULLUP);
   pinMode(BTN_LEFT, INPUT_PULLUP);
   pinMode(BTN_RIGHT, INPUT_PULLUP);
+
   pinMode(BTN_A, INPUT_PULLUP);
   pinMode(BTN_B, INPUT_PULLUP);
   pinMode(BTN_START, INPUT_PULLUP);
   pinMode(BTN_SELECT, INPUT_PULLUP);
-}
-
-/* ================= SETUP ================= */
-void setup() {
-  Serial.begin(115200);
 
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
 
-  setupButtons();
+  Serial.println("Starting GameBoy Emulator");
 
-  // Initialize emulator
-  gb_init(&gb, game_gb, game_gb_len);
+  gb_init(&gb, lcd_draw_line, audio_callback);
 
-  // Set LCD callback
-  gb.display.callback = lcd_draw_line;
+  gb_load_rom(&gb, game_rom);
+
 }
 
-/* ================= LOOP ================= */
-void loop() {
+// ---------------- LOOP ----------------
 
-  // Button mapping
-  gb.direct.joypad_bits.up     = (digitalRead(BTN_UP)     == LOW);
-  gb.direct.joypad_bits.down   = (digitalRead(BTN_DOWN)   == LOW);
-  gb.direct.joypad_bits.left   = (digitalRead(BTN_LEFT)   == LOW);
-  gb.direct.joypad_bits.right  = (digitalRead(BTN_RIGHT)  == LOW);
-  gb.direct.joypad_bits.a      = (digitalRead(BTN_A)      == LOW);
-  gb.direct.joypad_bits.b      = (digitalRead(BTN_B)      == LOW);
-  gb.direct.joypad_bits.start  = (digitalRead(BTN_START)  == LOW);
-  gb.direct.joypad_bits.select = (digitalRead(BTN_SELECT) == LOW);
+void loop()
+{
+  gb.direct.joypad = read_input();
 
-  // Run one frame
   gb_run_frame(&gb);
-
-  // Push framebuffer to TFT
-  tft.startWrite();
-  tft.setAddrWindow(40, 48, 160, 144);  // Centered on 320x240
-  tft.pushColors(framebuffer, 160 * 144, true);
-  tft.endWrite();
 }
